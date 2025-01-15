fc57b64da49ce6f2326be13a1cc134f5a2a67d96
gr3nnik <gabrielvdesteodoro@gmail.com>
2025-01-15 10:59:32 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(" .text", {
+    //Aqui eu posso alterar o letreiro que irá ficar atualizando com algumas informações minhas..
+    strings: ["Front-end Developer" , "Web Developer", "Testing Analyst" , "Integration Analyst"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    backDelay: 1500,
+    loop: true
+});
+
