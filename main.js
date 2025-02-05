ddc442b7a2bd4d5e5d7c73d928b42addc8d98dc5
SeuNomeDeUsuariogabrielvsteodoro <gabrielvs.teodoro@gmail.com>
2025-02-04 22:24:49 -0300

    Segundo Commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var typed = new Typed(" .text", {
     //Aqui eu posso alterar o letreiro que irá ficar atualizando com algumas informações minhas..
-    strings: ["Front-end Developer" , "Web Developer", "Testing Analyst" , "Integration Analyst"],
+    strings: ["Analista de Testes" , "Analista de QA", "Especialista em Qualidade de Software" , "Testador de Software"],
     typeSpeed: 100,
     backSpeed: 70,
     backDelay: 1500,
